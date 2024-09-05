dfbbc7244189c0e5af3f179f3e1f5fcac85382d1
Tanmay Bansal <tanmaybansal170@gmail.com>
2024-09-05 19:20:17 +0530

    Rules added

--- src/components/StartPage.jsx
+++ src/components/StartPage.jsx
@@ -9,14 +9,25 @@ const StartPage = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-800 text-white">
-      <h1 className="text-4xl m-2 font-bold">Hangman Game</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
+          <div className="text-center space-y-6">
+          <h1 className="text-4xl text-cyan-300 font-bold mb-4">Welcome to the Harry Potter Hangman</h1>
+          <div className="bg-gray-900 p-6 rounded-lg shadow-md text-left">
+            <h2 className="text-2xl font-semibold mb-4">Rules :</h2>
+            <ul className="list-disc list-inside space-y-2 text-lg">
+              <li>There will be a hint for every word.</li>
+              <li>You have to guess the word, and one point is awarded for every correct guess.</li>
+              <li>Each part of Hangman reflects a mistake.</li>
+              <li>Six tries will be given that show each part of hangman.</li>
+            </ul>
+          </div>
       <button
         onClick={startGame}
-        className="mt-8 px-6 py-3 bg-blue-600 text-white font-bold rounded hover:bg-blue-700"
+        className="bg-yellow-500 hover:bg-yellow-600 text-gray-900 font-bold py-2 px-6 rounded-lg transition-all"
       >
         Start Game
       </button>
+      </div>
     </div>
   );
 };
